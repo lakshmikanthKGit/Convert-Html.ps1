1681be3c9093531de845ce44fb32e3c0d5b891ab
laxmihub <laxmikanthforu@yahoo.com>
2016-06-20 06:06:35 -0700

    Update Exrcise1.ps1

--- Exrcise1.ps1
+++ Exrcise1.ps1
@@ -3,6 +3,7 @@ $c=$c+'th{background-color:Green;border:1px solid black}'
 $c=$c+'table {background-color: white; Padding: 5px;float:left;margin :5px;border-style:Solid}'
 $c=$c+'Td{background-color:Pink;Border-style: Solid;}'
 $c=$c+'</style>'
+$r=1
 $a=get-service|?{$_.Status -eq "running"}|Select Name,Status|ConvertTo-Html -Fragment
 $b=get-service|?{$_.Status -ne "running"}|Select Name,Status|ConvertTo-Html -Fragment
 ConvertTo-Html -Body "$a $B" -Head "$C"|Out-File C:\temp\test1.html
